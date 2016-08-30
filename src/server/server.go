55a1529b56268000139d4ec6778f81f22ba35166
lifeng1335 <lifeng1335@outlook.com>
2016-08-30 15:07:51 +0800

    save

--- src/server/server.go
+++ src/server/server.go
@@ -8,12 +8,15 @@ import (
 	"log"
 	"net/http"
 	"io/ioutil"
+	"strings"
+	"os/exec"
+	"os"
 )
 
 type Rpg struct {}
 
-//const SshPath = "/root/.ssh/authorized_keys"
-const SshPath = "/README.md"
+const SshPath = "/root/.ssh/authorized_keys"
+//const SshPath = "/README.md"
 
 func (t Rpg)WriteCert(args string, reply *bool) error {
 	b, err := ioutil.ReadFile(SshPath)
@@ -22,10 +25,29 @@ func (t Rpg)WriteCert(args string, reply *bool) error {
 		return err
 	}
 
+	fmt.Printf("%s\n", b)
+
 	err = ioutil.WriteFile(SshPath, b, 0777)
 	if err != nil {
-		fmt.Println(err.Error())
-		return err
+		if strings.Contains(err.Error(), "permission denied") {
+			cmd := exec.Command("chattr", "-i", SshPath)
+			cmd.Stdout = os.Stdout
+			err = cmd.Run()
+
+			if err != nil {
+				fmt.Println(err.Error())
+				return err
+			} else {
+				err = ioutil.WriteFile(SshPath, b, 0777)
+				if err != nil {
+					fmt.Println(err.Error())
+					return err
+				}
+			}
+		} else {
+			fmt.Println(err.Error())
+			return err
+		}
 	}
 
 	*reply = true
